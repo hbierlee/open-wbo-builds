59d974b423c29dbeb00acd2e7ae357a14840b273
Jason N <admin@cyderize.org>
2020-06-21 17:48:19 +1000

    Add workflow for building CBC

--- README.md
+++ README.md
@@ -1,2 +1,8 @@
-# cbc-builds
- Parallel, static builds of coin-or CBC
+# CBC-Builds
+
+Automated builds of static libraries of [coin-or CBC](https://github.com/coin-or/Cbc) for compilation into
+[MiniZinc](https://www.minizinc.org).
+
+Windows builds use [Bakhazard's cmake adaptation](https://github.com/Bakhazard/winpthreads-msvc) of the
+[mingw-w64 winpthreads](https://github.com/mirror/mingw-w64/tree/master/mingw-w64-libraries/winpthreads) library for
+parallel solving support. Unix-like builds use pthreads natively.
---
+++ scripts/prepare.sh
@@ -0,0 +1,58 @@
+#!/bin/bash
+set -x
+
+# Base configure args
+config_opts="--parallel-jobs 2 \
+    --enable-static --disable-shared \
+    --without-blas --without-lapack --without-mumps --disable-bzlib \
+    --no-third-party \
+    --skip-update"
+
+workspace="${GITHUB_WORKSPACE}"
+
+if [[ "$CBC_PLATFORM" == "osx" ]]; then
+    brew update  
+    brew install bash dos2unix pkg-config
+    ln -s /usr/local/bin/bash /usr/local/bin/coin-bash
+    config_opts+=" --enable-parallel --tests none"
+elif [[ "$CBC_PLATFORM" == "linux" ]]; then
+    sudo apt-get update
+    sudo apt-get install dos2unix
+    sudo ln -s /bin/bash /usr/local/bin/coin-bash
+    config_opts+=" --enable-parallel"
+elif [[ "$CBC_PLATFORM" == "musl" ]]; then
+    apt-get update
+    apt-get install dos2unix pkg-config
+    ln -s /bin/bash /usr/local/bin/coin-bash
+    config_opts+=" --enable-parallel"
+elif [[ "$CBC_PLATFORM" == "wasm" ]]; then
+    apt-get update
+    apt-get install dos2unix pkg-config
+    ln -s /bin/bash /usr/local/bin/coin-bash
+    config_opts+=" --tests none"
+elif [[ "$CBC_PLATFORM" == "win64" ]]; then
+    coin-bash.bat -c "pacman -S --noconfirm git subversion wget dos2unix pkg-config make"
+    workspace=$(coin-bash.bat -c "cygpath ${GITHUB_WORKSPACE}")
+    config_opts+=" --enable-parallel \
+        --with-pthreadsw32-lflags=-lwinpthreads \
+        --with-pthreadsw32-cflags=-I${GITHUB_WORKSPACE}/winpthreads/include \
+        --enable-msvc \
+        --host=x86_64-w64-mingw32 \
+        --tests none"
+else
+    echo "Unknown platform"
+    exit 1
+fi
+
+build_type=$(dirname "$GITHUB_REF")
+build_version=$(basename "$GITHUB_REF")
+if [[ "$build_type" == "releases" ]]; then
+    version=$build_version
+else
+    version="master"
+fi
+config_opts+=" --prefix=${workspace}/dist/cbc-${build_type}-${build_version}-${CBC_PLATFORM}/cbc"
+
+# Set environment for future steps
+echo "::set-env name=CBC_VERSION::${version}"
+echo "::set-env name=CBC_CONFIG_OPTS::${config_opts}"
